a6fae5d0c410a0d69c498a8163560c57d3638061
Cristina <2024_cristina.sanchez@iticbcn.cat>
2025-01-09 11:54:36 +0100

    sql

---
+++ fase2.sql
@@ -0,0 +1,9 @@
+/* *****************************************************
+  INSTITUT TIC de Barcelona
+    CFGS: DAW 1A
+    Mòdul: 0484 Bases de dades. 
+    AUTORS: Cristina Sánchez i Estefy Cevallos
+    DATA: 9/1/25
+****************************************************** */
+
+alter table __ add constraint _ primary key(__);
